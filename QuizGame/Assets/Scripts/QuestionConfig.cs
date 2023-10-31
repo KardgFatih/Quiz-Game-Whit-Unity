68fd7b69dcc13ecd2b51fa3eea1e6750926b739e
Fatih Karadağ <karadagfatih183@gmail.com>
2023-10-31 22:51:36 +0300

    Question Manager Added

--- QuizGame/Assets/Scripts/QuestionConfig.cs
+++ QuizGame/Assets/Scripts/QuestionConfig.cs
@@ -2,25 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Data;
+using System.Linq;
 
 public class QuestionConfig : MonoBehaviour
 {   
-    [SerializeField] private RoundDatas roundDatas; 
+    [SerializeField] private QuestionsDatas questionsDatas; 
     [SerializeField] private TextMeshProUGUI questionText;
     //ID = 0 => The text of the answer_1 button, ID = 1 => The text of the answer_2 button ...
-    [SerializeField] private TextMeshProUGUI[] answersText; 
+    [SerializeField] private TextMeshProUGUI[] answersText;
+
+    // questions[selectedID].QuestionsDatas = questions[selectedID].QuestionsDatas.OrderBy(i => Random.value).ToList();
+    public int currenntQuestionID;
 
     void Start()
     {
-        
+        MixOrder();
+        ConfigNextQuestion();
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currenntQuestionID += 1;
+            ConfigNextQuestion();
+        }
     }
 
     public void ConfigNextQuestion()
     {
+        if (questionsDatas.questionintheEpisode[currenntQuestionID] != null)
+        {
+            // Sahnedeki = textimiz scriptabledeki text
+            questionText.text = questionsDatas.questionintheEpisode[currenntQuestionID].questionText;
+
+            // s
+            for (int i = 0; i < questionsDatas.questionintheEpisode[currenntQuestionID].answer.Length ; ++i)
+            {
+                answersText[i].text = questionsDatas.questionintheEpisode[currenntQuestionID].answer[i];
+            }
+        }
+        else
+        {
+            //Eror or end;
+        }
+    }
+
+    public void MixOrder()
+    {
+        questionsDatas.questionintheEpisode = questionsDatas.questionintheEpisode.OrderBy(i => Random.value).ToList();
     }
 }
---
+++ QuizGame/Assets/Scripts/QuestionsDatas.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Question", menuName = "Questions")]
+public class QuestionsDatas : ScriptableObject
+{
+    public List<Questions> questionintheEpisode;
+}
+
+[Serializable]
+public class Questions
+{
+    [field: SerializeField] public string questionText { get; private set; }
+    [field: SerializeField] public string[] answer { get; private set; }
+    [field: SerializeField] public int corretAnswerID { get; private set; }
+}
